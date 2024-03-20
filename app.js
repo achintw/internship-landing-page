bb1d164ca3c39d57874c83d35aefb669a84e25b2
Achintya Tiwary <121821415+achintw@users.noreply.github.com>
2024-03-20 17:00:23 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -77,29 +77,43 @@ window.addEventListener("mousemove", function(e){
   
 });
 
-function animateCircles() {
-  
-  let x = coords.x;
-  let y = coords.y;
-  
-  circles.forEach(function (circle, index) {
-    circle.style.left = x - 12 + "px";
-    circle.style.top = y - 12 + "px";
-    
-    circle.style.scale = (circles.length - index) / circles.length;
-    
-    circle.x = x;
-    circle.y = y;
-
-    const nextCircle = circles[index + 1] || circles[0];
-    x += (nextCircle.x - x) * 0.3;
-    y += (nextCircle.y - y) * 0.3;
-  });
  
 
-  // recursive implementation, to counter the circles not merging when we exit the screen
-  requestAnimationFrame(animateCircles);
-}
+
+// console.log(screenWidth2 + "hello");
+
+
+
+    function animateCircles() {
+        var screenWidth2 = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        if(screenWidth2<1025){
+            circles.forEach((c)=>c.style.opacity = 0)
+        } else{
+            circles.forEach((c)=>c.style.opacity = 1)
+        }
+        let x = coords.x;
+        let y = coords.y;
+        
+        circles.forEach(function (circle, index) {
+          circle.style.left = x - 12 + "px";
+          circle.style.top = y - 12 + "px";
+          
+          circle.style.scale = (circles.length - index) / circles.length;
+          
+          circle.x = x;
+          circle.y = y;
+      
+          const nextCircle = circles[index + 1] || circles[0];
+          x += (nextCircle.x - x) * 0.3;
+          y += (nextCircle.y - y) * 0.3;
+        });
+       
+      
+        // recursive implementation, to counter the circles not merging when we exit the screen
+        requestAnimationFrame(animateCircles);
+      
+    }
+
 
 animateCircles();
 //-----------------------------------------------------------------------------------------------
